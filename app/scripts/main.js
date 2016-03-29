8ec528bd442e80d216e9a1c91b9e99815d9b4810
Ella.H@hotmail.de <Ella.H@hotmail.de>
2016-03-29 21:14:52 +0100

    added class to header on scroll for logo resizing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  $(window).on('scroll.sticky', function(){
+    console.log('triggered', $(window).scrollTop(), this);
+    if ($(window).scrollTop() > 200) {
+      console.log('in', $('.navbar-header'));
+			$('.navbar-header').addClass('hovering');
+		} else {
+      $('.navbar-header').removeClass('hovering');
+    }
+  });
+});
