575ffaabf4748721300ddbaa78e96f9e4fd052f7
Ella.H@hotmail.de <Ella.H@hotmail.de>
2016-04-05 21:01:25 +0100

    dump

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,4 +8,41 @@ $(function(){
       $('.navbar-header').removeClass('hovering');
     }
   });
+
+  function showImages(el) {
+          var windowHeight = $( window ).height();
+          $(el).each(function(){
+              var thisPos = $(this).offset().top;
+
+              var topOfWindow = $(window).scrollTop();
+              if (topOfWindow + windowHeight - 200 > thisPos ) {
+                  $(this).addClass('fadeIn');
+              } else {
+                  $(this).removeClass('fadeIn');
+              }
+          });
+      }
+
+      // if the image in the window of browser when the page is loaded, show that image
+      showImages('.block');
+
+
+      // if the image in the window of browser when scrolling the page, show that image
+      $(window).scroll(function() {
+              showImages('.block');
+      });
+
+      // function hideImages(el) {
+      //     var windowHeight = jQuery( window ).height();
+      //     $(el).each(function(){
+      //         var thisPos = $(this).offset().top;
+      //
+      //         var topOfWindow = $(window).scrollTop();
+      //         if (topOfWindow + windowHeight - 900 > thisPos ) {
+      //             $(this).addClass('fadeOut');
+      //         }
+      //     });
+      // }
+
+
 });
